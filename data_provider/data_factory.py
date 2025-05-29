4b5680bf16d01e403d89f1f434c05b05027d43cc
Nishan Khanal <nishan.khanal98@gmail.com>
2025-05-29 18:46:51 -0400

    add support for crime data

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,5 +1,5 @@
 from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_Solar, Dataset_PEMS, \
-    Dataset_Pred
+    Dataset_Pred, Dataset_Crime
 from torch.utils.data import DataLoader
 
 data_dict = {
@@ -10,6 +10,7 @@ data_dict = {
     'Solar': Dataset_Solar,
     'PEMS': Dataset_PEMS,
     'custom': Dataset_Custom,
+    'crime': Dataset_Crime,
 }
 
 
